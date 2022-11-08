5a15049140e973d65f7e7cd2e47c9b61a3016fe7
deoyrepo <117670176+deoyrepo@users.noreply.github.com>
2022-11-08 12:02:35 -0500

    Update index.js

--- api/quote/index.js
+++ api/quote/index.js
@@ -1,10 +1,9 @@
 const https = require('https');
 
-//const unplashApi = 'https://source.unsplash.com/1600x900?laptop';
-const unplashApi = 'https://images.unsplash.com/photo-1515343480029-43cdfe6b6aae?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bGFwdG9wfHx8fHx8MTY2NzkyNjIyNg&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600';
+const unplashApi = 'https://source.unsplash.com/1600x900?laptop';
 
 const quotes = [
-    'Market Place <br/>',
+    'Market Place \n',
     'Services & Consulting \n',
     'Business Process Automation'
 ];
