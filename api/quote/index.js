faaf8f0c67cac70767fb83799591e62aaab7ebf1
deoyrepo <117670176+deoyrepo@users.noreply.github.com>
2022-11-08 10:42:22 -0500

    Update index.js

--- api/quote/index.js
+++ api/quote/index.js
@@ -2,16 +2,17 @@ const https = require('https');
 
 const unplashApi = 'https://source.unsplash.com/1600x900?dream';
 const quotes = [
-  'Wherever you go, no matter what the weather, always bring your own sunshine.',
-  'You\’re awesome.',
-  'Happiness is the only thing that multiplies when you share it.',
-  'It always seems impossible until it is done.',
-  'Let your unique positive energy inspire confidence in others.',
-  'The best is yet to come.',
-  'You\'re capable of more than you can even dream.',
-  'You deserve the best.',
-  'Keep going, you\'re doing well.',
-  'Stay positive; stay hopeful'
+ // 'Wherever you go, no matter what the weather, always bring your own sunshine.',
+ // 'You\’re awesome.',
+ // 'Happiness is the only thing that multiplies when you share it.',
+ // 'It always seems impossible until it is done.',
+ // 'Let your unique positive energy inspire confidence in others.',
+ // 'The best is yet to come.',
+ // 'You\'re capable of more than you can even dream.',
+ // 'You deserve the best.',
+ // 'Keep going, you\'re doing well.',
+ // 'Stay positive; stay hopeful'
+   'DEOY MARKET - Servicios DEOY - BPA '
 ];
 
 async function getImage() {
